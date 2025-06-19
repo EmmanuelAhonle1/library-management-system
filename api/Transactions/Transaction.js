b09845119c58993450276a89052e1da461ddc08c
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2025-06-19 01:52:33 -0500

    Refactor Transaction class to consolidate transaction prefixes and simplify constructor parameters

--- api/Transactions/Transaction.js
+++ api/Transactions/Transaction.js
@@ -8,21 +8,57 @@ import AddItemTransaction from "./Librarian Transactions/AddItemTransaction";
 import DeleteItemTransaction from "./Librarian Transactions/DeleteItemTransaction";
 import UpdateItemTransaction from "./Librarian Transactions/UpdateItemTransaction";
 
+// Client Transaction Prefixes
+const CLIENT_TRANSACTION_PREFIXES = new Map([
+  ["chk", "checkout"],
+  ["ret", "return"],
+  ["hld", "hold"],
+  ["chl", "cancel_hold"],
+  ["rnw", "renewal"],
+  ["exp", "hold_expired"],
+]);
+
+// Librarian Transaction Prefixes
+const LIBRARIAN_TRANSACTION_PREFIXES = new Map([
+  ["add", "item_added"],
+  ["upd", "item_updated"],
+  ["del", "item_deleted"],
+]);
+
+// Combined Transaction Prefixes
+const TRANSACTION_PREFIXES = new Map([
+  ...CLIENT_TRANSACTION_PREFIXES,
+  ...LIBRARIAN_TRANSACTION_PREFIXES,
+]);
+
 class Transaction {
   transactionID;
-  transactionDate;
-  status;
   userID;
-  userType;
+  itemID;
+  transactionType;
   transactionPrefix;
 
-  constructor(transactionID, transactionDate, status, userID, userType) {
-    this.transactionID = transactionID;
-    this.transactionDate = transactionDate;
-    this.status = status;
+  constructor(userID, itemID, transactionPrefix = "") {
     this.userID = userID;
-    this.userType = userType;
-    this.transactionPrefix = "";
+    this.itemID = itemID;
+    this.transactionPrefix = transactionPrefix;
+    this.transactionType = TRANSACTION_PREFIXES.get(this.transactionPrefix);
+    this.transactionID = this.generateTransactionID(this.transactionPrefix);
+  }
+
+  generateTransactionID(transactionPrefix) {
+    // Generate 12 random alphanumeric characters (lowercase)
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let randomString = "";
+
+    for (let i = 0; i < 12; i++) {
+      randomString += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+
+    // Return format: prefix-randomString (e.g., "chk-abc123def456")
+    return `${transactionPrefix}-${randomString}`;
   }
 }
 
