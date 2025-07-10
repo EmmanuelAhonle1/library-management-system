4a7d228b64015bb54f9338ebc2e0243c6ac5e6f4
EmmanuelAhonle1 <emmanuelahonle101@gmail.com>
2025-07-10 06:48:15 -0500

    refactor: update dotenv configuration to use dynamic path resolution

--- api/Repositories/Repository.js
+++ api/Repositories/Repository.js
@@ -1,7 +1,12 @@
 import DatabaseHandler from "../DatabaseHandler/DatabaseHandler.js";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 DatabaseHandler;
 
 class Repository {
